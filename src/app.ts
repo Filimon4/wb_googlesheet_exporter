f4a27f59601f68d8e0f1f89ff465cfd3e7e95598
Filimon4 <f.rykov@bk.ru>
2025-08-18 18:48:05 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import '#config/env/env.js'
 import '#crons/index.js'
+
+// update data with google sheet for test at start
 import GoogleSheetService from '#modules/googleSheet/googleSheet.service.js'
+import WbService from '#modules/wb/wb.service.js'
 
-// await migrate.latest();
-// await seed.run();
-
+WbService.updateBoxTarrifs()
 GoogleSheetService.udpateTarrifsTable()
