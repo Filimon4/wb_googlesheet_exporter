894270924eaf019b86db78c4c78e90ee48d69b49
Filimon4 <f.rykov@bk.ru>
2025-08-18 21:18:23 +0300

    fix path

--- src/crons/wb.cron.ts
+++ src/crons/wb.cron.ts
@@ -1,5 +1,5 @@
 import cron from 'node-cron'
-import WbService from '../modules/wb/wb.service.js'
+import WbService from '#modules/wb/wb.service.js'
 import GoogleSheetService from '#modules/googleSheet/googleSheet.service.js'
 
 cron.schedule('0 0 * * * *', () => {
